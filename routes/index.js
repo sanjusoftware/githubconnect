629cae84e1439d5d936fd0751b2e3c5dd32d51dd
Sanjeev Mishra <sanjeev_mishra@mckinsey.com>
2013-12-16 09:08:37 +0530

    first cut of the app

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.helloworld = function(req, res){
+    res.render('helloworld', { title: 'Hello, World!' });
+};
+
+exports.userlist = function(db) {
+    return function(req, res) {
+        var collection = db.get('usercollection');
+        collection.find({},{},function(e,docs){
+            res.render('userlist', {
+                "userlist" : docs
+            });
+        });
+    };
+};
+
+exports.newuser = function(db) {
+    return function(req, res) {
+        var collection = db.get('usercollection');
+        collection.find({},{},function(e,docs){
+            res.render('userlist', {
+                "userlist" : docs
+            });
+        });
+    };
+};
